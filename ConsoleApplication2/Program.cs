aad2519a418a5acf46769a4a3e11642f005466d7
Sergey Kichuk <exiton3@gmail.com>
2018-09-26 02:23:01 +0300

    Update Program.cs

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -23,5 +23,35 @@ namespace ConsoleApplication2
             }
             ReadKey();
         }
-    }
+        
+        static string GetStringFromArray(int[] a)
+        {
+            string result = string.Empty;
+            Array.Sort(a);   
+            int start = a[0];
+            int end = a[0];
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] - a[i - 1] == 1)
+                {
+                    end = a[i];
+                }
+                else
+                {
+                    result += FormatString(start, end) + ",";
+                    start = a[i];
+                    end = a[i];
+                }
+            }
+
+            result += FormatString(start, end);
+
+            return result;
+        }
+
+        private static string FormatString(int start, int end)
+        {
+            return start == end ? start.ToString() : start + "-" + end;
+        }    }
 }
